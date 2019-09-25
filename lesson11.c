f6d104500d14d7e65c1935c394c77f4daadcb1d4
Brandon <bchen00@stuy.edu>
2019-09-25 04:52:06 +0100

    finished 3 problem

--- lesson11.c
+++ lesson11.c
@@ -11,12 +11,22 @@ int avg(int arr[], int size) {
   return sum/counter;
 }
 
-int copy(int arr1[], int arr2[], int size){
+int copy(int *arr1, int *arr2, int size){
   int i;
   for (i = 0; i < size; i++){
-    arr2[i] = arr1[i];
+    *(arr2 + i) = *(arr1 + i);
   }
 }
+
+int lengthOfWord(char *word){
+  int counter = 0;
+  while (word[counter] != '\0'){
+    counter++;
+  }
+  return counter;
+}
+
+
 
 int main(){
   int a1[] = {2,3,4,5,6};
@@ -32,4 +42,7 @@ int main(){
   for (j = 0; j < 5; j++){
     printf("%d ", a2[j]);
   }
+
+  printf("\n");
+  printf("The length of WORD is: %d", lengthOfWord("WORD"));
 }
