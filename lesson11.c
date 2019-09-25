05b2f5b36802150a5fface1e50dbafdd8e2815a7
Brandon <bchen00@stuy.edu>
2019-09-25 04:16:49 +0100

    got problem 1 working

--- lesson11.c
+++ lesson11.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-int avg(int arr[]) {
+int avg(int arr[], int size) {
   int sum = 0;
   int counter = 0;
   int i;
-  for (i = 0; i < sizeof(arr); i ++){
+  for (i = 0; i < size; i ++){
     counter++;
     sum += arr[i];
   }
@@ -12,7 +12,6 @@ int avg(int arr[]) {
 }
 
 int main(){
-  int a1[] = {1,2,3,4,5};
-  avg(a1);
-  printf("Problem 1 is: %d", avg(a1));
+  int a1[] = {2,3,4,5,6};
+  printf("Problem 1 is: %d", avg(a1,5));
 }
