c3b10a0baeb3c5ea81952aa849c4d93ac9447223
Brandon <bchen00@stuy.edu>
2019-09-25 04:26:58 +0100

    finished second problem

--- lesson11.c
+++ lesson11.c
@@ -11,7 +11,25 @@ int avg(int arr[], int size) {
   return sum/counter;
 }
 
+int copy(int arr1[], int arr2[], int size){
+  int i;
+  for (i = 0; i < size; i++){
+    arr2[i] = arr1[i];
+  }
+}
+
 int main(){
   int a1[] = {2,3,4,5,6};
-  printf("Problem 1 is: %d", avg(a1,5));
+  int a2[] = {};
+  printf("Problem 1 is: %d\n", avg(a1,5));
+  copy(a1,a2,5);
+  int i;
+  for (i = 0; i < 5; i++){
+    printf("%d ", a1[i]);
+  }
+  printf("\n");
+  int j;
+  for (j = 0; j < 5; j++){
+    printf("%d ", a2[j]);
+  }
 }
